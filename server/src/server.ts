deed8bee70ed6d255b297fa77534e14b05ff9ff4
Raphael Berger <raphaelmb@gmail.com>
2022-11-02 15:52:49 -0300

    Add db structure + routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import { z } from "zod";
 import { PrismaClient } from "@prisma/client";
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({
   log: ["query"],
@@ -20,7 +22,37 @@ async function bootstrap() {
     return { count };
   });
 
-  await fastify.listen({ port: 3333, /*host: "0.0.0.0"*/ });
+  fastify.get("/users/count", async () => {
+    const count = await prisma.user.count();
+    return { count };
+  });
+
+  fastify.get("/guesses/count", async () => {
+    const count = await prisma.guess.count();
+    return { count };
+  });
+
+  fastify.post("/pools", async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    const { title } = createPoolBody.parse(request.body);
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    return reply.status(201).send({ code });
+  });
+
+  await fastify.listen({ port: 3333 /*host: "0.0.0.0"*/ });
 }
 
 bootstrap();
